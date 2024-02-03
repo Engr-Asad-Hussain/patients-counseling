ddc8c2d167897602624b0c5ed0e9751d6c610a1d
Asad Hussain <asad.h1998@yahoo.com>
2024-02-03 22:40:47 +0500

    docs: added docstring

--- patients/views.py
+++ patients/views.py
@@ -18,6 +18,12 @@ from .models import Counsellor, Patient, Appointment
 
 @api_view(["GET", "POST"])
 def all_patients(request: Request):
+    """
+    Endpoint to handle the retrieval and creation of patients.\n
+    Args:
+        - request (Request): The HTTP request object.
+    """
+
     if request.method == "GET":
         # Collect all patients from models
         patients = Patient.objects.filter(is_active=True)
@@ -40,6 +46,15 @@ def all_patients(request: Request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def specific_patient(request: Request, uid: int):
+    """
+    Endpoint to retrieve, update, or delete a specific patient.\n
+    Args:
+        - request (Request): The HTTP request object.
+        - uid (int): The unique identifier of the patient returns from url-params.
+    Note:
+        - Deletion will result in `in_active` state to `False`
+    """
+
     try:
         patient = Patient.objects.get(pk=uid)
     except Patient.DoesNotExist:
@@ -68,7 +83,15 @@ def specific_patient(request: Request, uid: int):
 
 
 class CounsellorApi(APIView):
+    """ API endpoints for interacting with Counsellors. """
+
     def get(self, request: Request) -> Response:
+        """
+        Get endpoint to retrieve a list of active counsellors.\n
+        Args:
+            - request (Request): The HTTP request object.
+        """
+
         # Collect all counsellors from models
         counsellors = Counsellor.objects.filter(is_active=True)
 
@@ -77,6 +100,12 @@ class CounsellorApi(APIView):
         return Response(serializer.data)
 
     def post(self, request: Request) -> Response:
+        """
+        Post endpoint to create a new counsellor.\n
+        Args:
+            - request (Request): The HTTP request object.
+        """
+
         # Validate Schema
         request_body = request.data
         serializer = CreateCounsellorSerializer(data=request_body)
@@ -89,10 +118,21 @@ class CounsellorApi(APIView):
 
 
 class SpecificCounsellorApi(APIView):
+    """ API endpoint for interacting with specific Counsellors. """
+
     def _get_counsellor(self, pk: int) -> Counsellor:
+        """ Method to retrieve a specific counsellor by Primary-Key. """
+
         return Counsellor.objects.get(pk=pk)
 
     def get(self, request: Request, uid: int) -> Response:
+        """
+        Get endpoint to handle the retrieval of counsellor.\n
+        Args:
+            - request (Request): The HTTP request object.
+            - uid (int): The unique identifier of the counsellor returns from url-params.
+        """
+        
         try:
             counsellor = self._get_counsellor(uid)
         except Counsellor.DoesNotExist:
@@ -104,6 +144,13 @@ class SpecificCounsellorApi(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request: Request, uid: int) -> Response:
+        """
+        Put endpoint to handle the update of counsellor.\n
+        Args:
+            - request (Request): The HTTP request object.
+            - uid (int): The unique identifier of the counsellor returns from url-params.
+        """
+
         try:
             counsellor = self._get_counsellor(uid)
         except Counsellor.DoesNotExist:
@@ -120,6 +167,15 @@ class SpecificCounsellorApi(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request: Request, uid: int) -> Response:
+        """
+        Delete endpoint to handle the deletion of counsellor.\n
+        Args:
+            - request (Request): The HTTP request object.
+            - uid (int): The unique identifier of the counsellor returns from url-params.\n
+        Note:
+            - Deletion will result in `in_active` state to `False`
+        """
+
         try:
             counsellor = self._get_counsellor(uid)
         except Counsellor.DoesNotExist:
@@ -133,7 +189,21 @@ class SpecificCounsellorApi(APIView):
 
 
 class AppointmentApi(APIView):
+    """ API endpoint for interacting with appointments. """
+
     def get(self, request: Request) -> Response:
+        """
+        Get endpoint to retrieve a list of appointments based on optional query parameters.\n
+        Args:
+            - request (Request): The HTTP request object.\n
+        Query Parameters:
+            - `patient_id`: Retrieve appointments for a specific patient.
+            - `counsellor_id`: Retrieve appointments for a specific counsellor.
+            - `start_date`: Filter appointments starting from the specified date.
+            - `end_date`: Filter appointments until the specified date.
+            - `sort`: Sort appointments by date in ascending/descending order.
+        """
+
         patient_id = request.query_params.get("patient_id")
         counsellor_id = request.query_params.get("counsellor_id")
         if patient_id is not None:
@@ -182,6 +252,12 @@ class AppointmentApi(APIView):
         return Response(serializer.data)
 
     def post(self, request: Request) -> Response:
+        """
+        Post endpoint to create a new appointment.
+        Args:
+            - request (Request): The HTTP request object.
+        """
+
         request_body = request.data
         serializer = CreateAppointmentSerializer(data=request_body)
         if not serializer.is_valid():
@@ -192,10 +268,21 @@ class AppointmentApi(APIView):
 
 
 class SpecificAppointmentApi(APIView):
+    """ API endpoint for interacting with specific Appointments. """
+
     def _get_appointment(self, pk: int) -> Appointment:
+        """ Method to retrieve a specific appointment by Primary-Key. """
+
         return Appointment.objects.get(pk=pk)
 
     def get(self, request: Request, uid: int) -> Response:
+        """
+        Get endpoint to handle the retrieval of appointment.\n
+        Args:
+            - request (Request): The HTTP request object.
+            - uid (int): The unique identifier of the appointment returns from url-params.
+        """
+
         try:
             appointment = self._get_appointment(uid)
         except Appointment.DoesNotExist:
@@ -207,6 +294,15 @@ class SpecificAppointmentApi(APIView):
         return Response(serializer.data)
 
     def delete(self, request: Request, uid: int) -> Response:
+        """
+        Delete endpoint to handle the deletion of a.\n
+        Args:
+            - request (Request): The HTTP request object.
+            - uid (int): The unique identifier of the appointment returns from url-params.\n
+        Note:
+            - Deletion will result in `in_active` state to `False`
+        """
+
         try:
             appointment = self._get_appointment(uid)
         except Appointment.DoesNotExist:
