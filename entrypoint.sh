cb85fd806e1f4ae73a22e999c6ee20c5f0ed8a1f
Asad Hussain <asad.h1998@yahoo.com>
2024-02-03 21:36:01 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Wait for MySQL to be ready
 until (echo > /dev/tcp/$MYSQL_HOST/$MYSQL_PORT) >/dev/null 2>&1; do
   echo "Waiting for MySQL server to start..."
-  sleep 5
+  sleep 2
 done
 
 echo "MySQL is up and running. Starting Django application."
